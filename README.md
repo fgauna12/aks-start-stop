c9775022e81d0d25e4fee4823e8f763c83fa922d
Facundo Gauna <fgauna12@gmail.com>
2020-09-17 14:17:27 -0700

    Starting and stopping vmss for an AKS cluster

---
+++ README.md
@@ -0,0 +1,6 @@
+# Example Repo for Starting/Stopping an AKS Cluster
+
+:warning: Only use this on dev scenarios to save some money :money_bag:
+
+
+
---
+++ aks-start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CLUSTER_NAME=$1
+RESOURCE_GROUP=$2
+
+NODE_RESOURCE_GROUP=$(az aks show -g $RESOURCE_GROUP -n $CLUSTER_NAME --query "nodeResourceGroup" -o tsv)
+
+az vmss list -g $NODE_RESOURCE_GROUP --query "[].name" -o tsv | while read -r scale_set
+do 
+    echo "Starting scaleset in the AKS resource group. Scale Set: $scale_set"
+    az vmss start -g $NODE_RESOURCE_GROUP -n $scale_set
+done
+
+
